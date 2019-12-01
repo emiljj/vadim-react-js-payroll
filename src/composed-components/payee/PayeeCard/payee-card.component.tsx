3e676b7ba33db01677955fa91661b742ec24cdaa
vadik-rgb <trocenkovadik@gmail.com>
2019-12-01 17:27:06 +0200

    added styles

--- src/composed-components/payee/PayeeCard/payee-card.component.tsx
+++ src/composed-components/payee/PayeeCard/payee-card.component.tsx
@@ -12,16 +12,20 @@ const PayeeCard = (props: IPayeeCardProps) => {
   console.log('PAYEE => ', payee);
   return (
     <div className="payee-card">
-      <img className="payee-card__image" src="https://pickaface.net/gallery/avatar/20130828_022324_4848_Crispi.png" />
+      <img className="payee-card__image"
+       src="https://pickaface.net/gallery/avatar/20130828_022324_4848_Crispi.png" 
+      />
       <div className="payee-card__title">
         <div className="name__title">
           <h1>{`${payee.firstName} ${payee.lastName}`}</h1> 
-          <h4>{payee.jobTitle}</h4>
+          <div className="job-statuse__title"> 
+            <h4>{payee.jobTitle}</h4>
+          </div>
         </div>
         <div className="description__title">
-          <p>I like to make cool and creative designs.</p>
-          <p>My design stash is always full of refreshing ideas.</p>
-          <p>Feel free to take a look around my Vcard.</p>
+          <p>I like to make cool and creative designs.
+          My design stash is always full of refreshing ideas.
+          Feel free to take a look around my Vcard.</p>
         </div>
       </div>
       <div className="payee-card__information">
