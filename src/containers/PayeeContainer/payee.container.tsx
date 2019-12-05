345b3bc275039427dc828d44925b1ff04d6a8ccb
Emil Kulmuhametov <emilkulmuhametov@Emils-MacBook-Pro.local>
2019-12-05 19:39:06 +0200

    added header, fixed styles

--- src/containers/PayeeContainer/payee.container.tsx
+++ src/containers/PayeeContainer/payee.container.tsx
@@ -16,13 +16,35 @@ class PayeeContainer extends React.Component<IPayeeContainerProps, IPayeeContain
     payees: testData,
     activeId: null
   }
+
+  calculatePayeesTotalSalary = () => {}
+
+  findUserByRole = () => {}
+
+  findHighestSalary = () => {}
   
   render() {
     return (
       <div className="payee-container">
+        <div className="payee-container__header">
+          <div>
+            <p>Payees count: {0} </p> 
+          </div>
+          <div>
+            <p>Total salary: {0} </p> 
+          </div>
+          <div>
+            <p>Admin: {'Admin Name'}</p>
+          </div>
+          <div>
+            <p>Highest salary: {1000}</p>
+          </div>
+        </div>
+        <div className="payee-container__payees-list">
         {this.state.payees.map((payee: IPayee) => {
           return <PayeeCard key={payee.id} payee={payee} />
         })}
+        </div>
       </div>
     );
   }
