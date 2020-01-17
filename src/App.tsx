db43bfc56c0de76a8e6fa96c252cbded0f0d12ad
vadik-rgb <trocenkovadik@gmail.com>
2020-01-17 18:43:59 +0200

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PayeesContainer from './containers/PayeesContainer/payees.container';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <div className="app">
-      <PayeesContainer />
+      <BrowserRouter>
+        <Route path="/payees" component={PayeesContainer} />
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
